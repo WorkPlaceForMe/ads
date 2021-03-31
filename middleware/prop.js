faab9c161b14a3fdeadafc25a807b3d125fabf52
Abhratanu Mukherjee <abhratanu@graymatics.com>
2021-03-31 12:45:59 +0530

    property reader fix and axios added

--- middleware/prop.js
+++ middleware/prop.js
@@ -1,6 +1,6 @@
-const PropertiesReader = require('properties-reader');
-const conf_path = process.env.CONF_FILE_PATH
-const prop = PropertiesReader(conf_path);
-exports.get = function(key){
-    return prop.get(key);
+const PropertiesReader = require('properties-reader')
+// const conf_path = process.env.CONF_FILE_PATH
+const prop = PropertiesReader('app.properties')
+exports.get = function (key) {
+	return prop.get(key)
 }
