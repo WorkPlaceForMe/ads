0d7b604af889ec68b059e4df74b3b98682aa8bb5
Alex Kaiser <i93kaiser@hotmail.com>
2021-05-06 14:26:37 -0400

    Cleaned reader

--- controllers/readCsv.js
+++ controllers/readCsv.js
@@ -1,25 +1,6 @@
-const conf = require('../middleware/prop')
-const Controller = require('../helper/controller')
 const csv = require('csv-parser')
 const fs = require('fs')
-const path = require('path')
 
-
-exports.readCsv = Controller(async(req, res) => {
-const results = [];
-
-fs.createReadStream('./csv/file.csv')
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-    console.log(results[0]);
-    res.status(200).json({success: true, data: results[0]})
-    // [
-    //   { NAME: 'Daffy Duck', AGE: '24' },
-    //   { NAME: 'Bugs Bunny', AGE: '22' }
-    // ]
-  });
-})
 
 exports.readCsv = new Promise(async (resolve, reject) =>{
 
