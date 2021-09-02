50e5bd295e3435051016ca1407c7af3671374cf1
Rodrigo <rodrigo@graymatics.com>
2021-09-02 14:47:40 -0400

    test1

--- controllers/ads.js
+++ controllers/ads.js
@@ -58,7 +58,7 @@ exports.getAds = Controller(async(req, res) => {
                 try{
                     const response = await axios(request_config)
 
-                console.log('=====================> VISTA RESPONSE <========================')
+                console.log('=====================> VISTA sdfsdfdRESPONSE <========================')
                 //     console.log(response.data.results)
                 //console.log(util.inspect(response.data, false, null, true))
                 let resultsVista = []
