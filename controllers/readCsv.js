a9b21e132a3848b6a1abc187746ec360c5d67d46
Rodrigo <rodrigo@graymatics.com>
2021-11-02 18:09:28 -0300

    fix bug

--- controllers/readCsv.js
+++ controllers/readCsv.js
@@ -13,7 +13,6 @@ const products = require('../campaigns-db/models/products');
 const { stringify } = require('uuid');
 const clothing = require('../campaigns-db/models/clothing');
 const { resolve } = require('path');
-
 const arrObjetos = Object.keys(objetos[0])
 const WomenClothes = Object.keys(objetos[1]['Women Clothes'])
 const MenClothes = Object.keys(objetos[1]["Men Clothes"])
@@ -46,30 +45,20 @@ exports.readCsv = async function (idPbl) {
           }
         )
         // for(const id in ids){
-
-        let campaignEndpoint = `${conf.get('accesstrade_endpoint')}/v1/publishers/me/sites/${idPbl}/campaigns/affiliated`
-        
+        let affiliateEndpoint = `${conf.get('accesstrade_endpoint')}/v1/publishers/me/sites/${idPbl}/campaigns/677/productfeed/url`
         try {
-          const campaignResponse = await axios.get(campaignEndpoint, {
+          const affiliateResponse = await axios.get(affiliateEndpoint, {
             headers: {
               'Authorization': `Bearer ${token}`,
               'X-Accesstrade-User-Type': 'publisher'
             }
           })
-
           console.log(`Downloading shopee`)
           const rs = await download(affiliateResponse.data.baseUrl, idPbl)
 
           const results = await readCsv(rs, idPbl)
           resolve(results)
 
-
-              } catch (err) {
-                console.error(err)
-              }
-            }
-          }
-          resolve(results)
         } catch (err) {
           console.error(err)
         }
@@ -102,8 +91,7 @@ async function download(url) {
   })
 }
 
-async function readCsv(id) {
-  let path = `./csv/${id}.csv`
+async function readCsv(path, id) {
   return new Promise((resolve, reject) => {
 
     path
@@ -143,7 +131,6 @@ async function readCsv(id) {
       })
       .on('end', async function () {
         await fs.promises.writeFile(`./csv/${id}.json`, JSON.stringify(objetos, null, 2), 'utf-8');
-        fs.unlinkSync(path)
         await cache.setAsync(`downloading-${id}`, false);
         console.log(`Done with shopee`)
         const objetos_json = objetos
