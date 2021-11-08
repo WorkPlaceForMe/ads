7b4416f53ffc46306332a04b81cbc5baa3ec48fe
Rodrigo <rodrigo@graymatics.com>
2021-11-08 16:26:45 -0300

    not stable workin on it

--- controllers/readCsv.js
+++ controllers/readCsv.js
@@ -78,85 +78,20 @@ exports.readCsv = async function (idPbl) {
 }
 
 async function download(url) {
-  return new Promise((resolve, reject) => {
-    axios.get(url).then(resp => {
-      const Csv = Readable.from(resp.data)
-      resolve(Csv)
-    }).catch(err => {
-      reject(console.error(err))
-    })
-  })
+  const resp = await axios.get(url)
+  const Csv = Readable.from(resp.data)
+  return (Csv)
 }
 
 async function readCsv(path, id) {
-  return new Promise((resolve) => {
-    path
-      .pipe(parseCsv({ delimiter: ',', from_line: 2, headers: true }))
-      .on('data', async function (csvrow) {
-        // aqui mandar a vista cada row
-        for (const element of arrObjetos) {
-          if (csvrow[15].toLowerCase().includes(" " + element) || csvrow[15].toLowerCase() == element) {
-            objetos[0][element].push(csvrow)
-            await products.create({
-              Merchant_Product_Name: csvrow[1],
-              Image_URL: csvrow[2],
-              Product_URL_Web_encoded: csvrow[4],
-              Product_URL_Mobile_encoded: csvrow[5],
-              Description: csvrow[6],
-              Price: csvrow[7],
-              Descount: csvrow[8],
-              Available: csvrow[9],
-              Main_Category_Name: csvrow[13],
-              Category_Name: csvrow[15],
-              Sub_Category_Name: csvrow[17],
-              Price_Unit: csvrow[18],
-              label: element
-            }).catch((err) => {
-              console.error('algo fallo', err)
-            })
-          } else if (csvrow[17].toLowerCase().includes(" " + element) || csvrow[17].toLowerCase() == element) {
-            objetos[0][element].push(csvrow)
-            await products.create({
-              Merchant_Product_Name: csvrow[1],
-              Image_URL: csvrow[2],
-              Product_URL_Web_encoded: csvrow[4],
-              Product_URL_Mobile_encoded: csvrow[5],
-              Description: csvrow[6],
-              Price: csvrow[7],
-              Descount: csvrow[8],
-              Available: csvrow[9],
-              Main_Category_Name: csvrow[13],
-              Category_Name: csvrow[15],
-              Sub_Category_Name: csvrow[17],
-              Price_Unit: csvrow[18],
-              label: element
-            }).catch((err) => {
-              console.error('algo fallo', err)
-            })
-          } else if (csvrow[13].toLowerCase().includes(" " + element) || csvrow[13].toLowerCase() == element) {
-            objetos[0][element].push(csvrow)
-            await products.create({
-              Merchant_Product_Name: csvrow[1],
-              Image_URL: csvrow[2],
-              Product_URL_Web_encoded: csvrow[4],
-              Product_URL_Mobile_encoded: csvrow[5],
-              Description: csvrow[6],
-              Price: csvrow[7],
-              Descount: csvrow[8],
-              Available: csvrow[9],
-              Main_Category_Name: csvrow[13],
-              Category_Name: csvrow[15],
-              Sub_Category_Name: csvrow[17],
-              Price_Unit: csvrow[18],
-              label: element
-            }).catch((err) => {
-              console.error('algo fallo', err)
-            })
-          }
-        }
+  path
+    .pipe(parseCsv({ delimiter: ',', from_line: 2, headers: true }))
+    .on('data', async function (csvrow) {
+      // aqui mandar a vista cada row
+      for (const element of arrObjetos) {
         if (csvrow[15] == 'Mobile') {
           objetos[0]['cell_phone'].push(csvrow)
-          await products.create({
+          const product = await products.create({
             Merchant_Product_Name: csvrow[1],
             Image_URL: csvrow[2],
             Product_URL_Web_encoded: csvrow[4],
@@ -170,117 +105,110 @@ async function readCsv(path, id) {
             Sub_Category_Name: csvrow[17],
             Price_Unit: csvrow[18],
             label: 'cell_phone'
-          }).catch((err) => {
-            console.error('algo fallo', err)
-          })
+          }).dataValues
         }
-        if (csvrow[13] == 'Women Clothes' || csvrow[15] == 'Women Sportswear') {
-          for (const Garment of WomenClothes) {
-            if (csvrow[15].toLowerCase().includes(" " + Garment) || csvrow[15].toLowerCase().includes(Garment)) {
-              objetos[1]['Women Clothes'][Garment].push(csvrow)
-              await clothing.create({
-                Merchant_Product_Name: csvrow[1],
-                Image_URL: csvrow[2],
-                Product_URL_Web_encoded: csvrow[4],
-                Product_URL_Mobile_encoded: csvrow[5],
-                Description: csvrow[6],
-                Price: csvrow[7],
-                Descount: csvrow[8],
-                Available: csvrow[9],
-                Main_Category_Name: csvrow[13],
-                Category_Name: csvrow[15],
-                Sub_Category_Name: csvrow[17],
-                Price_Unit: csvrow[18],
-                label: {
-                  gender: 'Female',
-                  garment: Garment
-                }
-              }).catch((err) => {
-                console.error('algo fallo con la ropa ', err)
-              })
-            } else if (csvrow[17].toLowerCase().includes(" " + Garment) || csvrow[17].toLowerCase().includes(Garment)) {
-              objetos[1]['Women Clothes'][Garment].push(csvrow)
-              await clothing.create({
-                Merchant_Product_Name: csvrow[1],
-                Image_URL: csvrow[2],
-                Product_URL_Web_encoded: csvrow[4],
-                Product_URL_Mobile_encoded: csvrow[5],
-                Description: csvrow[6],
-                Price: csvrow[7],
-                Descount: csvrow[8],
-                Available: csvrow[9],
-                Main_Category_Name: csvrow[13],
-                Category_Name: csvrow[15],
-                Sub_Category_Name: csvrow[17],
-                Price_Unit: csvrow[18],
-                label: {
-                  gender: 'Female',
-                  garment: Garment
-                }
-              }).catch((err) => {
-                console.error('algo fallo con la ropa ', err)
-              })
+        else if (csvrow[15].toLowerCase().includes(" " + element) || csvrow[15].toLowerCase() == element) {
+          objetos[0][element].push(csvrow)
+          const product = await create_products(element).dataValues
+        } else if (csvrow[17].toLowerCase().includes(" " + element) || csvrow[17].toLowerCase() == element) {
+          objetos[0][element].push(csvrow)
+          const product = await create_products(element).dataValues
+        } else if (csvrow[13].toLowerCase().includes(" " + element) || csvrow[13].toLowerCase() == element) {
+          objetos[0][element].push(csvrow)
+          const product = await create_products(element).dataValues
+        }
+      }
+      if (csvrow[13] == 'Women Clothes' || csvrow[15] == 'Women Sportswear') {
+        for (const Garment of WomenClothes) {
+          if (csvrow[15].toLowerCase().includes(" " + Garment) || csvrow[15].toLowerCase().includes(Garment)) {
+            objetos[1]['Women Clothes'][Garment].push(csvrow)
+            const garment = await create_female_clothing(Garment).dataValues
+          } else if (csvrow[17].toLowerCase().includes(" " + Garment) || csvrow[17].toLowerCase().includes(Garment)) {
+            objetos[1]['Women Clothes'][Garment].push(csvrow)
+            const garment = await create_female_clothing(Garment).dataValues
+          }
+        }
+      }
+      if (csvrow[13] == 'Men Clothes' || csvrow[15] == 'Men Sportswear') {
+        for (const Garment of MenClothes) {
+          if (csvrow[15].toLowerCase().includes(" " + Garment) || csvrow[15].toLowerCase().includes(Garment)) {
+            objetos[1]['Men Clothes'][Garment].push(csvrow)
+            const garment = await create_male_clothing(Garment).dataValues
+          } else if (csvrow[17].toLowerCase().includes(" " + Garment) || csvrow[17].toLowerCase().includes(Garment)) {
+            objetos[1]['Men Clothes'][Garment].push(csvrow)
+            const garment = await create_male_clothing(Garment).dataValues
+          }
+        }
+      }
+    })
+    .on('end', async function () {
+      await fs.promises.writeFile(`./csv/${id}.json`, JSON.stringify(objetos, null, 2), 'utf-8');
+      console.log(`Done with shopee`)
+      console.log("uploading to Mysql")
+      await cache.setAsync(`downloading-${id}`, false);
+      return (objetos)
+    });
+}
 
-            }
-          }
-        }
-        if (csvrow[13] == 'Men Clothes' || csvrow[15] == 'Men Sportswear') {
-          for (const Garment of MenClothes) {
-            if (csvrow[15].toLowerCase().includes(" " + Garment) || csvrow[15].toLowerCase().includes(Garment)) {
-              objetos[1]['Men Clothes'][Garment].push(csvrow)
-              await clothing.create({
-                Merchant_Product_Name: csvrow[1],
-                Image_URL: csvrow[2],
-                Product_URL_Web_encoded: csvrow[4],
-                Product_URL_Mobile_encoded: csvrow[5],
-                Description: csvrow[6],
-                Price: csvrow[7],
-                Descount: csvrow[8],
-                Available: csvrow[9],
-                Main_Category_Name: csvrow[13],
-                Category_Name: csvrow[15],
-                Sub_Category_Name: csvrow[17],
-                Price_Unit: csvrow[18],
-                label: {
-                  gender: 'Male',
-                  garment: Garment
-                }
-              }).catch((err) => {
-                console.error('algo fallo con la ropa ', err)
-              })
-            } else if (csvrow[17].toLowerCase().includes(" " + Garment) || csvrow[17].toLowerCase().includes(Garment)) {
-              objetos[1]['Men Clothes'][Garment].push(csvrow)
-              await clothing.create({
-                Merchant_Product_Name: csvrow[1],
-                Image_URL: csvrow[2],
-                Product_URL_Web_encoded: csvrow[4],
-                Product_URL_Mobile_encoded: csvrow[5],
-                Description: csvrow[6],
-                Price: csvrow[7],
-                Descount: csvrow[8],
-                Available: csvrow[9],
-                Main_Category_Name: csvrow[13],
-                Category_Name: csvrow[15],
-                Sub_Category_Name: csvrow[17],
-                Price_Unit: csvrow[18],
-                label: {
-                  gender: 'Male',
-                  garment: Garment
-                }
-              }).catch((err) => {
-                console.error('algo fallo con la ropa ', err)
-              })
-            }
-          }
-        }
-      })
-      .on('end', async function () {
-        await fs.promises.writeFile(`./csv/${id}.json`, JSON.stringify(objetos, null, 2), 'utf-8');
-        console.log(`Done with shopee`)
-        const objetos_json = objetos
-        console.log("uploading to Mysql")
-        await cache.setAsync(`downloading-${id}`, false);
-        resolve(objetos)
-      });
+const create_products = async (element) => {
+  const product = await products.create({
+    Merchant_Product_Name: csvrow[1],
+    Image_URL: csvrow[2],
+    Product_URL_Web_encoded: csvrow[4],
+    Product_URL_Mobile_encoded: csvrow[5],
+    Description: csvrow[6],
+    Price: csvrow[7],
+    Descount: csvrow[8],
+    Available: csvrow[9],
+    Main_Category_Name: csvrow[13],
+    Category_Name: csvrow[15],
+    Sub_Category_Name: csvrow[17],
+    Price_Unit: csvrow[18],
+    label: element
   })
+  return (product)
+}
+
+const create_female_clothing = async (element) => {
+  const garment = await clothing.create({
+    Merchant_Product_Name: csvrow[1],
+    Image_URL: csvrow[2],
+    Product_URL_Web_encoded: csvrow[4],
+    Product_URL_Mobile_encoded: csvrow[5],
+    Description: csvrow[6],
+    Price: csvrow[7],
+    Descount: csvrow[8],
+    Available: csvrow[9],
+    Main_Category_Name: csvrow[13],
+    Category_Name: csvrow[15],
+    Sub_Category_Name: csvrow[17],
+    Price_Unit: csvrow[18],
+    label: {
+      gender: 'Female',
+      garment: Garment
+    }
+  })
+  return (garment)
+}
+
+const create_male_clothing = async (element) => {
+  const garment = await clothing.create({
+    Merchant_Product_Name: csvrow[1],
+    Image_URL: csvrow[2],
+    Product_URL_Web_encoded: csvrow[4],
+    Product_URL_Mobile_encoded: csvrow[5],
+    Description: csvrow[6],
+    Price: csvrow[7],
+    Descount: csvrow[8],
+    Available: csvrow[9],
+    Main_Category_Name: csvrow[13],
+    Category_Name: csvrow[15],
+    Sub_Category_Name: csvrow[17],
+    Price_Unit: csvrow[18],
+    label: {
+      gender: 'Male',
+      garment: Garment
+    }
+  })
+  return (garment)
 }
