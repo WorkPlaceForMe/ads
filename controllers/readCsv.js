a6611a3cb0abbdc1e19a8cafe740812adb1c63d4
Rodrigo <rodrigo@graymatics.com>
2021-11-22 11:52:24 -0300

    readCsv opti

--- controllers/readCsv.js
+++ controllers/readCsv.js
@@ -11,132 +11,93 @@ const products = db.products
 const clothing = db.clothing
 
 exports.readCsv = async function (idPbl) {
-  const val1 = await db.sequelize.query(`SELECT EXISTS (SELECT 1 FROM ${conf.get('database')}.products);`)
-  const val2 = await db.sequelize.query(`SELECT EXISTS (SELECT 1 FROM ${conf.get('database')}.clothings);`)
   let cachedDown = await cache.getAsync(`downloading-${idPbl}`);
-  if (Object.values(val1[0][0])[0] == 1 && Object.values(val2[0][0])[0] == 1) {
-    const Clothing = await clothing.findAll({
+  const val1 = products.count({whare})
+  const val2 = clothing.count()
+  const enter = await Promise.all([val1,val2])
+  if (enter[0] >0 && enter[1] >1) {
+    const Clothing = clothing.findAll({
       raw: true
     })
-    const Products = await products.findAll({
+    const Products = products.findAll({
       raw: true
     })
-    const dataValues = Clothing.concat(Products)
-    
-    return dataValues
-  }else {
-    console.log(cachedDown)
-    if (cachedDown == 'false' || !cachedDown) {
+    const dataValues = await Promise.all([Clothing, Products])
+    const flat = flatten(dataValues)
+    return flat
+  }
+  else if (cachedDown == 'false' || !cachedDown) {
       await cache.setAsync(`downloading-${idPbl}`, true);
-      // return new Promise((resolve, reject) => {
-        const ids = {
-          shopee: 677
-        }
-        try{
-          const credentials = await aff.getAff()
-          const token = jwt.sign(
-            { sub: credentials.userUid },
-            credentials.secretKey,
-            {
-              algorithm: "HS256"
-            }
-          )
-          let affiliateEndpoint = `${conf.get('accesstrade_endpoint')}/v1/publishers/me/sites/${idPbl}/campaigns/677/productfeed/url`
-          axios.get(affiliateEndpoint, {
-            headers: {
-              'Authorization': `Bearer ${token}`,
-              'X-Accesstrade-User-Type': 'publisher'
-            }
-          }).then((affiliateResponse) => {
-            console.log(`Downloading ${idPbl}`)
-            download(affiliateResponse.data.baseUrl, idPbl)
-              .then((rs) => {
-                readCsv(rs, idPbl)
-                  .then((results) =>{
-                    return(results)}).catch((err) => {
-                      console.error(err)
-                      return(err)
-                    })
-              })
-          })
-        }catch(err){
-          console.log(err)
-        }
-        // aff.getAff.then(async function (credentials) {
-        //   const token = jwt.sign(
-        //     { sub: credentials.userUid },
-        //     credentials.secretKey,
-        //     {
-        //       algorithm: "HS256"
-        //     }
-        //   )
-        //   let affiliateEndpoint = `${conf.get('accesstrade_endpoint')}/v1/publishers/me/sites/${idPbl}/campaigns/677/productfeed/url`
-        //   axios.get(affiliateEndpoint, {
-        //     headers: {
-        //       'Authorization': `Bearer ${token}`,
-        //       'X-Accesstrade-User-Type': 'publisher'
-        //     }
-        //   }).then((affiliateResponse) => {
-        //     download(affiliateResponse.data.baseUrl, idPbl)
-        //       .then((rs) => {
-        //         readCsv(rs, idPbl)
-        //           .then((results =>
-        //             resolve(results))).catch((err) => {
-        //               console.error(err)
-        //               reject(err)
-        //             })
-        //       })
-        //   })
-        // })
-      // })
-    }else {
+      const ids = {
+        shopee: 677
+      }
+      try {
+        const credentials = await aff.getAff()
+        const token = jwt.sign(
+          { sub: credentials.userUid },
+          credentials.secretKey,
+          {
+            algorithm: "HS256"
+          }
+        )
+        let affiliateEndpoint = `${conf.get('accesstrade_endpoint')}/v1/publishers/me/sites/${idPbl}/campaigns/677/productfeed/url`
+        const affiliateResponse = await axios.get(affiliateEndpoint, {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'X-Accesstrade-User-Type': 'publisher'
+          }
+        })
+        const rs = await download(affiliateResponse.data.baseUrl, idPbl)
+        const results = await readCsv(rs, idPbl)
+        return results
+      } catch (err) {
+        console.log(err)
+      }
+    } else {
       cachedDown = await cache.getAsync(`downloading-${idPbl}`)
       while (cachedDown == 'true') {
         cachedDown = await cache.getAsync(`downloading-${idPbl}`)
         continue;
       }
-      const Clothing = await clothing.findAll({
+      const Clothing = clothing.findAll({
         raw: true
       })
-      const Products = await products.findAll({
+      const Products = products.findAll({
         raw: true
       })
-      const dataValues = Clothing.concat(Products)
-      return dataValues
+      const dataValues = await Promise.all([Clothing, Products])
+      const flat = flatten(dataValues)
+      return flat
     }
-  }
 }
 
-async function readCsv(path, id) {
+async function readCsv(Readable, id) {
   const promises = []
-  path
+  console.log('piping')
+  var sendDate = (new Date()).getTime();
+  Readable
     .pipe(parseCsv({ delimiter: ',', from_line: 2, headers: true }))
-    .on('data', async function (csvrow) {
+    .on('data', function (csvrow) {
       for (const element of objetos['Products']) {
         if (csvrow[15].toLowerCase().includes(" " + element) || csvrow[15].toLowerCase() == element) {
           const product = create_products(csvrow, element, id)
-          // dataValues['products'].push(product.dataValues)
           promises.push(product)
         } else if (csvrow[17].toLowerCase().includes(" " + element) || csvrow[17].toLowerCase() == element) {
           const product = create_products(csvrow, element, id)
-          // dataValues['products'].push(product.dataValues)
           promises.push(product)
         } else if (csvrow[13].toLowerCase().includes(" " + element) || csvrow[13].toLowerCase() == element) {
           const product = create_products(csvrow, element, id)
-          // dataValues['products'].push(product.dataValues)
           promises.push(product)
         }
       }
       if (csvrow[13] == 'Women Clothes') {
         const gender = "Female"
         const garment = create_clothing(csvrow, id, gender)
-        // dataValues['clothing'].push(garment.dataValues)
         promises.push(garment)
       }
       if (csvrow[13] == 'Men Clothes') {
         const gender = 'Male'
         const garment = create_clothing(csvrow, id, gender)
-        // dataValues['clothing'].push(garment.dataValues)
         promises.push(garment)
       }
       if (csvrow[13] == 'Sports & Outdoors' && !csvrow[15].includes('Sportswear')) {
@@ -154,11 +115,26 @@ async function readCsv(path, id) {
     })
     .on('end', async () => {
       const todo = await Promise.all(promises)
-      const dataValues = todo.map(objects => objects.dataValues)
       console.log('done with shopee')
+      var receiveDate = (new Date()).getTime();
+      var responseTimeMs = receiveDate - sendDate;
+      console.log(responseTimeMs)
+      await cache.setAsync(`downloading-${id}`, false);
+      const dataValues = todo.map(objects => objects.dataValues)
       await cache.setAsync(`downloading-${id}`, false);
       return dataValues
     });
+}
+
+const download = async (url, idPbl) => {
+  var sendDate = (new Date()).getTime();
+  console.log(`Downloading ${idPbl}`)
+  const resp = await axios.get(url)
+  const Csv = Readable.from(resp.data)
+  var receiveDate = (new Date()).getTime();
+  var responseTimeMs = receiveDate - sendDate;
+  console.log(responseTimeMs)
+  return (Csv)
 }
 
 const create_products = (csvrow, element, id) => {
@@ -203,8 +179,12 @@ const create_clothing = (csvrow, id, gender) => {
   return (garment)
 }
 
-const download = async (url) => {
-  const resp = await axios.get(url)
-  const Csv = Readable.from(resp.data)
-  return (Csv)
+const flatten = ary =>{
+  return ary.reduce((a, b)=>{
+      if (Array.isArray(b)) {
+          return a.concat(flatten(b))
+      }
+      return a.concat(b)
+  }, [])
 }
+
