36299d1a0d074d69d1d4de9343800e9890566549
Alex Kaiser <i93kaiser@hotmail.com>
2021-11-24 17:16:23 -0300

    makeup fixed

--- controllers/ads.js
+++ controllers/ads.js
@@ -112,7 +112,8 @@ const filler = (resultsVista, serv, img_width, img_height, site, url, uid, objet
                 }
             }
         }
-        else if (resultsVista[0].tags2.tags2.tags2[0].label.includes("LIPSTICK" || "HAIR" || "FACE" || "PERFUME" || "PAINTBRUSH") || resultsVista[0].tags2.tags2.tags2[0].IAB.includes("IAB17-")) {
+        else if (resultsVista[0].tags2.tags2.tags2.length != 0){
+            if(resultsVista[0].tags2.tags2.tags2[0].label.includes("LIPSTICK" || "HAIR" || "FACE" || "PERFUME" || "PAINTBRUSH") || resultsVista[0].tags2.tags2.tags2[0].IAB.includes("IAB17-")) {
             const bool = false
             for (const obj of resultsVista[0].tags2.tags2.tags2) {
                 const result = sport_makeup_Filler(bool, obj, objetos, serv, img_width, img_height, site, url, uid, mobile)
@@ -120,6 +121,7 @@ const filler = (resultsVista, serv, img_width, img_height, site, url, uid, objet
                     resultsAffiliate.push(result)
                 }
             }
+        }
         }
         if (resultsVista[0]['fashion'].length != 0) {
             if (resultsVista[0]['face'].length != 0 && resultsVista[0]['fashion'][0].confidence > 0.6) {
