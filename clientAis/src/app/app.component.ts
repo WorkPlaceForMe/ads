e542f5245390bd80fdeb6dacd2dbd5c868e80d42
Alex Kaiser <i93kaiser@hotmail.com>
2022-02-14 12:39:51 +0000

    css

--- clientAis/src/app/app.component.ts
+++ clientAis/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { SeoService } from './@core/utils/seo.service';
 @Component({
   selector: 'ngx-app',
   template: '<router-outlet></router-outlet>',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
