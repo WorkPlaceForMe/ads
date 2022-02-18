c31f4165edacc1b06418d968708fda106afeb8b4
Alex Kaiser <i93kaiser@hotmail.com>
2022-02-18 14:17:19 +0000

    Checker for duplicates updated

--- middleware/verifyPubl.js
+++ middleware/verifyPubl.js
@@ -1,13 +1,32 @@
 const db = require('../campaigns-db/database')
 const Publishers = db.publishers;
+const { Op } = require("sequelize");
 
 let checkDuplicatePubl = (req, res, next) => {
   // Username
+  let name;
+  if(req.body.name.startsWith("http")){
+    name = req.body.name.split('http')[1]
+    if(name.includes("www.")){
+      name = name.split('www.')[1]
+    }else{
+      name = req.body.name.split('//')[1]
+    }
+  }else{
+    if(req.body.name.startsWith("www.")){
+      name = req.body.name.split('www.')[1]
+    }else{
+      name = req.body.name
+    }
+  }
   Publishers.findOne({
     where: {
-      name: req.body.name
+      name: {
+        [Op.substring]: name
+      }
     }
   }).then(user => {
+    console.log(user)
     if (user) {
       res.status(400).send({ success: false,
         repeated: 'name',
