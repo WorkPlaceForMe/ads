d6a6cc99272bb6bcf1c68985dcc310e6207c8971
Ranjit Bhakta <ranjit3t@gmail.com>
2022-10-13 15:25:01 +0530

    Modifying API

--- clientAis/src/app/models/API.ts
+++ clientAis/src/app/models/API.ts
@@ -1,15 +1,5 @@
-import { ip } from './IpServer';
 import { environment } from "../../environments/environment";
 
-// var  url = '/api';
-// var  url = 'http://'+ ip +':3310/api';
+const  url = '/api'
 
-let url
-if (environment.production === false) {
-    url = "http://" + ip + ":3300/api";
-  } else {
-    url = "/api";
-  }
-  
-
-export var api: string = url;
+export var api: string = url
